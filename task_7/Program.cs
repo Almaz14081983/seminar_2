51bf4c209fa77e1643a496f595b3319633b9a8bc
Almaz <minnexanov.a@mail.ru>
2023-01-31 19:33:55 +0300

    task_7

---
+++ task_7/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+//
+
+Console.Clear();
+int count = 1;
+while(count <=100)
+{
+    if((count%3 == 0) & (count%5 == 0))
+    {
+         Console.Write("FizzBuzz");
+    }
+    else if(count%3 == 0)
+    {
+         Console.Write("Fizz");
+    }
+    else if(count%5 == 0)
+    {
+         Console.Write("Buzz");
+    }
+    else
+    {
+        Console.Write(count);
+    }
+    if (count == 100)
+    {
+        Console.Write(".");
+    }
+    else
+    {
+        Console.Write(", ");
+    }
+    count++;
+}
+

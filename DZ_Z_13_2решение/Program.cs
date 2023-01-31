e2ec1e061097ce94d5f16c3d9d1b79d1cba5037a
Almaz <minnexanov.a@mail.ru>
2023-01-31 22:01:17 +0300

    DZ_Z_13_2решение

---
+++ DZ_Z_13_2решение/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.Clear();
+int a1 = new Random().Next(10,100000);
+Console.WriteLine($"Случайное число: {a1}");
+// Console.WriteLine("Введите число  ");
+// int a1 = int.Parse(Console.ReadLine());
+if((9<a1) & (a1<100))
+{
+    Console.WriteLine("третьей цифры нет");
+}
+if((100<a1) & (a1<1000))
+{
+    int a2 = a1%10;
+    Console.WriteLine($"цифра: {a2}");
+}
+if((999<a1) & (a1<10000))
+{
+    int a3 = a1%100;
+    int a4 = a3/10;
+    Console.WriteLine($"цифра: {a4}");
+}
+if((9999<a1) & (a1<100000))
+{
+    int a5 = a1%1000;
+    int a6 = a5/100;
+    Console.WriteLine($"цифра: {a6}");
+}
+
+
